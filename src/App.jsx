7eaad39ba000398d3bf26f23ddd8d633c004f9e8
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2024-05-24 20:18:49 +0600

    Ai universe full project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import Header from './components/Header/Header';
+import Button from './components/Button/Button';
+import Cards from './components/Cards/Cards';
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <Cards></Cards>
+    </>
+  );
+};
+
+export default App;
